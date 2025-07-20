5910a418cd4a1f1720ebd89f8e8eebb1eb567bfb
yoshiki0123 <yoshiki.tamehira1212@gmail.com>
2025-07-20 23:37:45 +0900

    208

---
+++ ABC-contests/abc208/A/main.py
@@ -0,0 +1,26 @@
+#library
+import sys, re, heapq
+from math import sin, cos, radians, ceil, floor, sqrt, isqrt, pi, gcd, comb
+from collections import deque, Counter, defaultdict
+from bisect import bisect, bisect_left, bisect_right
+from functools import lru_cache
+from itertools import permutations, combinations, product
+
+sys.setrecursionlimit(10**6)
+INF = float('inf')
+
+#class
+
+#def
+
+#main
+def main():
+    A, B = map(int,input().split())
+    if A <= B and B <= A*6:
+        print('Yes')
+    else:
+        print('No')
+    
+
+if __name__ == '__main__':
+    main()
