cc551fc152081ed7c824b549f625aa22c3f1e936
yoshiki0123 <yoshiki.tamehira1212@gmail.com>
2025-07-30 21:19:08 +0900

    218

---
+++ ABC-contests/abc218/B/main.py
@@ -0,0 +1,29 @@
+#library
+import sys, re, heapq
+from math import (
+sin, cos, radians, ceil, 
+floor, sqrt, isqrt, pi, 
+gcd, comb, factorial
+)
+from collections import deque, Counter, defaultdict
+from bisect import bisect, bisect_left, bisect_right
+from functools import lru_cache
+from itertools import permutations, combinations, product
+
+sys.setrecursionlimit(10**6)
+INF = float('inf')
+
+#class
+
+#def
+
+#main
+def main():
+    P = list(map(int, input().split(' ')))
+    ans = ''
+    for p in P:
+        ans += chr(p + ord('a') - 1 )
+    print(ans)    
+
+if __name__ == '__main__':
+    main()
